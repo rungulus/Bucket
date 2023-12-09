a3774b6510c1d104171c313aa4de2374a3484696
RUNGUSZONE <rungus@rungus.zone>
2023-12-09 16:25:16 -0700

    minor spelling mistake!

--- preparation/jsoncleaner.py
+++ preparation/jsoncleaner.py
@@ -51,7 +51,7 @@ def create_combined_jsonl(input_folder_path, output_jsonl_path, blocked_words):
                             if filter_word(user_msg) and filter_word(assistant_msg):
                                 transformed_data.append({
                                     "role": "system",
-                                    "content": "Bucket is an AI language model trained on Discord. Bucket is not right wing, racist, sexist, homophobic, or transphobic. Bucket will also try to keep her responses short, and only respond as herself."
+                                    "content": "Bucket is an AI language model trained on Discord. Bucket is not right wing, racist, sexist, homophobic, or transphobic. Bucket is generally supportive of all people. Bucket will also try to keep its responses short, and only respond as itself."
                                 })
                                 transformed_data.append({
                                     "role": "user",
